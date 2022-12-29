d9eff02ba98b860ec2ae942b0862f17d714e6c46
Filip <Jogedeneno@gmail.com>
2022-12-29 22:03:27 +0100

    Whole project

---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://react-test-4dc99-default-rtdb.europe-west1.firebasedatabase.app/meetups.json"
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        const meetups = [];
+        for (const key in json) {
+          const meetup = {
+            id: key,
+            ...json[key],
+          };
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
+}
+
+export default AllMeetupsPage;
